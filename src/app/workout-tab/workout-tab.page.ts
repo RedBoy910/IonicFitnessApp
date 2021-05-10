67c4bea3f375b2b510785836515f2ed263e105f2
Tudor Pasca <tudorpasca11@gmail.com>
2021-05-10 22:14:11 +0300

    semi-functional slider

--- src/app/workout-tab/workout-tab.page.ts
+++ src/app/workout-tab/workout-tab.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonSlide, IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-workout-tab',
@@ -6,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workout-tab.page.scss'],
 })
 export class WorkoutTabPage implements OnInit {
+  currentSegment = 0;
+  currentSlide: IonSlides;
+  slideOptions = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    speed: 400
+  }
 
   constructor() { }
+
+  async segmentChanged(ev){
+    await this.currentSlide.slideTo(this.currentSegment);
+  }
+
+  async slideChanged(slide: IonSlides){
+    this.currentSlide = slide;
+    slide.getActiveIndex().then((index) => {
+        this.currentSegment = index;
+        console.log("slide");
+     });
+  }
 
   ngOnInit() {
   }
